d5331c015f15d14905479fdd4cc25678666bb6df
odhis101 <joshodhiambo5@gmail.com>
2024-03-25 11:32:35 +0300

    Initial Commit

---
+++ EFTA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EFTA
+//
+//  Created by Joshua on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EFTA/EFTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  EFTAApp.swift
+//  EFTA
+//
+//  Created by Joshua on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct EFTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
